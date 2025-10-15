42beed39c340436feecdaef2bafcd0f32458379d
Kadir-Saliu <kadir_saliu@hotmail.de>
2025-10-15 15:41:39 +0200

    fixed character, chicken and background

--- models/world.class.js
+++ models/world.class.js
@@ -1,31 +1,43 @@
 class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
-  clouds = [new Cloud()]
+  clouds = [new Cloud()];
   canvas;
+  backgroundObjects = [
+    new BackgroundObject("img/5_background/layers/air.png",0),
+    new BackgroundObject("img/5_background/layers/3_third_layer/1.png",0),
+     new BackgroundObject("img/5_background/layers/2_second_layer/1.png",0),
+     new BackgroundObject("img/5_background/layers/1_first_layer/1.png",0),
+  ];
   ctx;
-  constructor(canvas){
-    this.ctx = canvas.getContext('2d');
+  constructor(canvas) {
+    this.ctx = canvas.getContext("2d");
     this.canvas = canvas;
     this.draw();
   }
 
   draw() {
-    this.ctx.clearRect(0,0,this.canvas.width, this.canvas.height)
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-    this.ctx.drawImage(this.character.img, this.character.x, this.character.y,this.character.width,this.character.height);
-
-    this.enemies.forEach(enemy => {
-      this.ctx.drawImage(enemy.img, enemy.x, enemy.y,enemy.width,enemy.height);
-    });
-
-     this.clouds.forEach(cloud => {
-      this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-    });
+     this.addObjectToMap(this.backgroundObjects);
+    this.addToMap(this.character);
+    this.addObjectToMap(this.clouds);
+    this.addObjectToMap(this.enemies);
+   
 
     let self = this;
-    requestAnimationFrame(function() {
+    requestAnimationFrame(function () {
       self.draw();
-    })
+    });
   }
-}
+
+  addToMap(mo) {
+    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+  }
+
+  addObjectToMap(objects) {
+    objects.forEach(o => {
+      this.addToMap(o);
+    });
+  }
+} 
