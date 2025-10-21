dd2ac3f7fa8849c5ba0d43b65fb6bd668918ea4c
Kadir-Saliu <kadir_saliu@hotmail.de>
2025-10-21 23:29:38 +0200

    add walk of the character left and right

--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,7 @@
 class Character extends MovableObject {
   height = 280;
   y = 155;
+  speed = 10;
   IMAGES_WALKING = [
     "../img/2_character_pepe/2_walk/W-21.png",
     "../img/2_character_pepe/2_walk/W-22.png",
@@ -9,20 +10,34 @@ class Character extends MovableObject {
     "../img/2_character_pepe/2_walk/W-25.png",
     "../img/2_character_pepe/2_walk/W-26.png",
   ];
- 
+  world;
 
   constructor() {
     super().loadImage("../img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
     this.animate();
   }
-  animate(){
-    setInterval(()=> {
-      let i = this.currentImage % this.IMAGES_WALKING.length;
+  animate() {
+
+    setInterval(()=>{
+      if(this.world.keyboard.RIGHT){
+        this.x += this.speed;
+      }
+
+       if(this.world.keyboard.LEFT){
+        this.x -= this.speed;
+      }
+      
+    },1000/60)
+
+    setInterval(() => {
+      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+        let i = this.currentImage % this.IMAGES_WALKING.length;
         let path = this.IMAGES_WALKING[i];
         this.img = this.imageChache[path];
         this.currentImage++;
-    },100)
+      }
+    }, 50);
   }
 
   jump() {}
