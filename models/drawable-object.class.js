a29ec584dabfaa2df6bf2c85e06a2414c053cdbf
Kadir-Saliu <kadir_saliu@hotmail.de>
2025-11-19 12:05:01 +0100

    add try and catch for the draw and add the  responsive

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -14,7 +14,16 @@ class DrawableObject {
   };
 
   draw(ctx) {
-    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+
+    try {
+      ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    } catch (error) {
+      console.warn('Error loading image', error);
+      console.log('Could not load image', this.img.src);
+      
+    }
+
+    
   };
 
   
